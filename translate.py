731f7852a9c23b32220d392568f5bcf1ff7dcc88
Kelly Zhang <kellywzhang@hotmail.com>
2018-03-04 00:59:43 -0500

    no attention in beam

--- translate.py
+++ translate.py
@@ -85,6 +85,8 @@ def main():
 
     # Translator
     scorer = onmt.translate.GNMTGlobalScorer(opt.alpha, opt.beta)
+    if model_opt.global_attention == 'none':
+        scorer = None
     translator = onmt.translate.Translator(model, fields,
                                            beam_size=opt.beam_size,
                                            n_best=opt.n_best,
@@ -93,7 +95,8 @@ def main():
                                            copy_attn=model_opt.copy_attn,
                                            cuda=opt.cuda,
                                            beam_trace=opt.dump_beam != "",
-                                           min_length=opt.min_length)
+                                           min_length=opt.min_length,
+                                           use_attn=True if model_opt.global_attention != 'none' else False)
     builder = onmt.translate.TranslationBuilder(
         data, translator.fields,
         opt.n_best, opt.replace_unk, opt.tgt)
